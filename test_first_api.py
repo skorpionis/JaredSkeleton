63a6407daa69d8977fcfa7b4eb03b308c26626f6
skorpionis <skorp9393@mail.ru>
2022-08-08 09:58:02 -0500

    added skeleton

---
+++ test_first_api.py
@@ -0,0 +1,29 @@
+import requests
+from httpretty import HTTPretty
+from httpretty.core import HTTPrettyRequest
+from lib.assertions import Assertions
+from lib.base_case import BaseCase
+
+
+class TestUserAuth(BaseCase):
+    def setup(self):
+        data = {'email': 'sdsd@mail.ru',
+                'password': '1234'
+                }
+        response = requests.get("URL", data=data)
+        self.auth_sid = self.get_cookie(response, "auth_sid")
+        self.token = self.get_header(response, "x-csrf-token")
+        self.user_id_from_auth_method = self.get_json_value(response, "user_id")
+
+
+    def test_auth_user(self):
+        response = requests.get("URL",
+                                headers={"x-csrf-token": self.token},
+                                cookies={"auth_sid": self.auth_sid}
+                                )
+        Assertions.assert_json_value_by_name(
+            response,
+            "user_id",
+            self.user_id_from_auth_method,
+            "User id from auth method is not equal to user id from check method"
+        )
