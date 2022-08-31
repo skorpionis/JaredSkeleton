b23fd6f351204ad1c30bbfe3ae4ba63a4c24ad4a
skorpionis <skorp9393@mail.ru>
2022-08-31 10:08:53 -0500

    gathered map with urls and headers

--- lib/file_parser.py
+++ lib/file_parser.py
@@ -1,38 +1,35 @@
-from pathlib import Path
-
-import pandas as pd
 import json
+from pathlib import Path
 
 # filepath = "12.txt"
 filepath = Path("../", "files/example.txt")
 
 with open(filepath, 'r') as file:
     for line in file.readlines():
-        data_pan = []
-        # print(line)
-        if "accept-encoding: " in line:
-            encode = line.split(": ")[1]
-            print(encode)
-        if "content-type: " in line:
-            content = line.split(": ")[1]
-            print(content)
-        if "user-agent: " in line:
-            agent = line.split(": ")[1]
-            print(agent)
-        if "connection: " in line:
-            connection = line.split(": ")[1]
-            print(connection)
+        if line != "" and "api" not in line:
+            headers_array = []
+            # print(line)
+            if "accept-encoding: " in line:
+                encode = line.split(": ")[1]
+            if "content-type: " in line:
+                content = line.split(": ")[1]
+            if "user-agent: " in line:
+                agent = line.split(": ")[1]
+            if "connection: " in line:
+                connection = line.split(": ")[1]
+
         if "{\\" in line:
-            yy = line.encode('utf-8').decode('ascii', 'ignore')
-            y = json.loads(yy)
-            y = json.loads(y)
-            print(type(y))
+            if "true" and "false" not in line:
+                before_decode = line.encode('utf-8').decode('ascii', 'ignore')
+                body = json.loads(before_decode)
+                body = json.loads(body)
 
     row = {"accept-encoding": encode,
            "content-type": content,
            "user-agent": agent,
            "connection": connection}
 
-    data_pan.append(row)
+    headers_array.append(row)
+    map_result_body = {"headers": row, "body": body}
 
-    data = pd.DataFrame(data_pan)
+    # row data = pd.DataFrame(data_pan)
