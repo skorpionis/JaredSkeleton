adcf190fed4818ab4cb7fa1ac08bf29686c3b14e
skorpionis <skorp9393@mail.ru>
2022-09-06 17:30:58 -0500

    fixed closing the file and redundant if condition

--- lib/file_parser_new.py
+++ lib/file_parser_new.py
@@ -12,16 +12,13 @@ def map_http_method_url():
         mas_text = []
         array = file.readlines()
         for line in array:
-            if 'HTTP' in line:
-                file.close()
             if line == "\n":
-                file.close()
                 break
             mas_text.append(line.strip("\n"))
-        for i, word1 in enumerate(array):
-            for word2 in array[i + 2:]:
-                if word1 == "\n" and word2 == "\n" and array[i - 1].startswith('"{\\"'):
-                    body = array[i - 1]
+    for i, word1 in enumerate(array):
+        for word2 in array[i + 2:]:
+            if word1 == "\n" and word2 == "\n" and array[i - 1].startswith('"{\\"'):
+                body = array[i - 1]
     return dict(http_method=res[0], url=res[1], headers=mas_text, body=body)
 
 
